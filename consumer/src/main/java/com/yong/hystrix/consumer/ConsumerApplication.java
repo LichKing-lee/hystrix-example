5325315ba6cdc4427d1fbc2a9e8c43a6eeb8d6d0
ChangYong <lcy9002@naver.com>
2018-12-23 17:22:39 +0900

    Be configured eureka client

--- consumer/src/main/java/com/yong/hystrix/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/yong/hystrix/consumer/ConsumerApplication.java
@@ -3,6 +3,7 @@ package com.yong.hystrix.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication
 public class ConsumerApplication {
